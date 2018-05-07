9b47908911442eea26531ee8213f24b559df9d3a
Dmitry Skorlupkin <daemych@gmail.com>
2018-05-07 21:11:39 +0300

    test commit

---
+++ NavApp2/NavApp2/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Sensors;
+using Windows.Devices.Enumeration;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace NavApp2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SimpleOrientationSensor simpleOrientationSensor;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            simpleOrientationSensor = SimpleOrientationSensor.GetDefault();
+            if(simpleOrientationSensor!=null)
+            simpleOrientationSensor.OrientationChanged += SimpleOrientationSensor_OrientationChanged;
+        }
+
+        async private void SimpleOrientationSensor_OrientationChanged(SimpleOrientationSensor sender, SimpleOrientationSensorOrientationChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                tblSimpleOrientation.Text = args.Orientation.ToString();
+            });
+        }
+
+    }
+}
